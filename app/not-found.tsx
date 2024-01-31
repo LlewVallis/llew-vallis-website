18f0ba4abec3e9536c0ba3e0983ba72bad3786f5
LlewVallis <llewvallis@gmail.com>
2024-01-31 14:35:03 +1000

    Add 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="text-center mt-[25vh]">
+      <h1 className="fredoka font-semibold text-4xl mb-2">Page not found</h1>
+      <p className="text-lg">
+        This page, doesn&apos;t exist.
+        {" "}
+        <Link href="/" className="text-pink-600 font-medium hover:underline">
+          Go home
+        </Link>
+        .
+      </p>
+    </div>
+  );
+}
